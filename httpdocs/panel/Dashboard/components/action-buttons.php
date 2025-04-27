3afb4fba160f3629da921dc9f363bcf743c8d77f
arodoo <haro.em@hotmail.com>
2025-04-26 19:54:32 -0600

    feat: Refactor action buttons layout for improved responsiveness and styling

--- httpdocs/panel/Dashboard/components/action-buttons.php
+++ httpdocs/panel/Dashboard/components/action-buttons.php
@@ -1,39 +1,51 @@
-<div class="row">
-    <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
-        <div class="card h-100">
-            <div class="card-body text-center">
-                <i class="fas fa-search fa-3x mb-3 text-primary"></i>
-                <h4>Dépanneurs</h4>
-                <p>Trouvez des dépanneurs près de chez vous</p>
-                <a href="/Plateforme/Depanneurs/1" class="btn btn-primary btn-lg btn-block">
-                    Rechercher un dépanneur
-                </a>
+<div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+    <div class="col">
+        <div class="card h-100 shadow-sm">
+            <div class="card-body d-flex flex-column text-center p-4">
+                <div class="mb-auto">
+                    <i class="fas fa-search fa-3x mb-3 text-primary"></i>
+                    <h4 class="card-title">Dépanneurs</h4>
+                    <p class="card-text">Trouvez des dépanneurs près de chez vous</p>
+                </div>
+                <div class="mt-3">
+                    <a href="/Plateforme/Depanneurs/1" class="btn btn-primary w-100">
+                        Rechercher un dépanneur
+                    </a>
+                </div>
             </div>
         </div>
     </div>
     
-    <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
-        <div class="card h-100">
-            <div class="card-body text-center">
-                <i class="fa fa-car-crash fa-3x mb-3 text-warning"></i>
-                <h4>Mes constats</h4>
-                <p>Accédez à vos constats amiables</p>
-                <a href="/Constats" class="btn btn-warning btn-lg btn-block">
-                    Voir mes constats
-                </a>
+    <div class="col">
+        <div class="card h-100 shadow-sm">
+            <div class="card-body d-flex flex-column text-center p-4">
+                <div class="mb-auto">
+                    <i class="fa fa-car-crash fa-3x mb-3 text-warning"></i>
+                    <h4 class="card-title">Mes constats</h4>
+                    <p class="card-text">Accédez à vos constats amiables</p>
+                </div>
+                <div class="mt-3">
+                    <a href="/Constats" class="btn btn-primary w-100">
+                        Voir mes constats
+                    </a>
+                </div>
             </div>
         </div>
     </div>
     
-    <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
-        <div class="card h-100">
-            <div class="card-body text-center">
-                <i class="fas fa-file-alt fa-3x mb-3 text-success"></i>
-                <h4>Mes documents</h4>
-                <p>Gérez vos documents automobiles</p>
-                <a href="/Mes-documents" class="btn btn-success btn-lg btn-block">
-                    Voir mes documents
-                </a>
+    <div class="col">
+        <div class="card h-100 shadow-sm">
+            <div class="card-body d-flex flex-column text-center p-4">
+                <div class="mb-auto">
+                    <i class="fas fa-file-alt fa-3x mb-3 text-success"></i>
+                    <h4 class="card-title">Mes documents</h4>
+                    <p class="card-text">Gérez vos documents automobiles</p>
+                </div>
+                <div class="mt-3">
+                    <a href="/Mes-documents" class="btn btn-primary w-100">
+                        Voir mes documents
+                    </a>
+                </div>
             </div>
         </div>
     </div>
